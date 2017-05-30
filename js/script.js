f4d9e4f2803bf7ee4daea5fa8f2c83bcb7231032
Eduard Endovickiy <greeny0909@gmail.com>
2017-05-30 17:57:28 +0300

    ex11 function sumTo() and fib()

--- js/script.js
+++ js/script.js
@@ -13,6 +13,13 @@ function sumToo(b) {
 	return a;
 }
 
+function fib(a) {
+
+	if (a <= 1) {
+		return a;
+	} else return fib(a - 2) + fib(a - 1);
+}
+
 var a = 99;
 document.writeln(sumTo(a));
 
@@ -22,3 +29,6 @@ document.writeln(sumToo(b));
 var c = 99;
 var d = c * (c + 1) / 2;
 document.writeln(d);
+
+var x = 7;
+document.writeln(fib(x));
