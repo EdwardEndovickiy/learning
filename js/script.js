2935b745285b0a37279dcbc0f4efb77a707f1266
Eduard Endovickiy <greeny0909@gmail.com>
2017-05-30 16:04:53 +0300

    ex8 function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-		var a = +prompt("a", "");
+function exampleFun(a) {
+	switch (a){
+		case 0:
+			alert(0);
+			break;
+		case 1:
+			alert(1);
+			break;
+		case 2:
+		case 3:
+			alert("2, 3");
+			break;
+		default: 
+			alert("NO!");
+			break;		
+	}
+}
 
-		switch (a){
-			case 0:
-				alert(0);
-				break;
-			case 1:
-				alert(1);
-				break;
-			case 2:
-			case 3:
-				alert("2, 3");
-				break;
-
-			default: 
-				alert("NO!");
-				break;		
-		}
+var a = +prompt("a", "");
+exampleFun(a);
