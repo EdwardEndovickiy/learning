0e1bd1e38d33f162f7dbd26b41dd35f343e5c3dd
Eduard Endovickiy <greeny0909@gmail.com>
2017-05-30 14:59:17 +0300

    ex7 while <100

--- js/script.js
+++ js/script.js
@@ -3,13 +3,7 @@
 		// (res >= 18 && res < 21) ? alert("Уже совершеннолетний, я смотрю") :
 		// (res >= 21) ? alert("Стареешь") : alert("Привет");
 
-		var i = 0;
-		var res = true;
-
-		bre: while(i) {
-			res = prompt("Введите число больше 100 ", "");
-			if(res<100) break bre
-			alert(res);
-			i = false;
-		}
-		
+		var num;
+		do {
+			num = prompt("Введите число: ", "")
+		} while (num <= 100 && num !=null)
