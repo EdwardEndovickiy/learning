e6f862d4dba861fa9a5c45c591edacc406172134
Eduard Endovickiy <greeny0909@gmail.com>
2017-05-30 12:40:52 +0300

    ex3 prompt

---
+++ js/script.js
@@ -0,0 +1,6 @@
+		var userName = "Василий";
+		var myEarth = "Земля";
+		alert(typeof myEarth);
+		alert(userName + " живет на планете "+ myEarth);
+		var res = prompt("сколько вам лет?", 100);
+		alert(res);
